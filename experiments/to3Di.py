838ae6f7c1791cbd50ae8b0acfc718a95c388838
xnought <bertuccd@oregonstate.edu>
2024-02-26 13:36:36 -0800

    feat: new data and more intuitive function

--- experiments/to3Di.py
+++ experiments/to3Di.py
@@ -1,5 +1,6 @@
 import subprocess
 from dataclasses import dataclass
+import shutil
 import os
 
 
@@ -42,7 +43,18 @@ class Parsed3DiAA:
     repr_3Di: list[str]
 
 
+def parse_foldseekdb_for_3Di(db):
+    _, names = parse_names(db)
+    repr_3Di = parse_seqs(db + "_ss")
+    repr_AA = parse_seqs(db)
+    assert (
+        len(repr_3Di) == len(repr_AA) == len(names)
+    ), "The number of sequences should be the same."
+    return Parsed3DiAA(names=names, repr_amino_acids=repr_AA, repr_3Di=repr_3Di)
+
+
 def to3Di(dir="~/Desktop/proteins", db="./.temp/3DiAA", verbose=False):
+    # create db
     os.makedirs(".temp", exist_ok=True)
     try:
         foldseek(f"createdb {dir} {db}", verbose=verbose)
@@ -50,14 +62,13 @@ def to3Di(dir="~/Desktop/proteins", db="./.temp/3DiAA", verbose=False):
         print(e)
         return
 
-    _, names = parse_names(db)
-    repr_3Di = parse_seqs(db + "_ss")
-    repr_AA = parse_seqs(db)
-    assert (
-        len(repr_3Di) == len(repr_AA) == len(names)
-    ), "The number of sequences should be the same."
+    # parse db
+    parsed = parse_foldseekdb_for_3Di(db)
 
-    return Parsed3DiAA(names=names, repr_amino_acids=repr_AA, repr_3Di=repr_3Di)
+    # remove db
+    shutil.rmtree(".temp")
+
+    return parsed
 
 
 if __name__ == "__main__":
