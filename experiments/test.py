3fc5c5f228156259ce76f129e3d26a2743d39e6b
xnought <bertuccd@oregonstate.edu>
2024-02-26 13:28:25 -0800

    feat: make more concise

---
+++ experiments/test.py
@@ -0,0 +1,5 @@
+from to3Di import to3Di, Parsed3DiAA
+
+if __name__ == "__main__":
+    d = to3Di("../data")
+    print(d)
--- experiments/to3Di.py
+++ experiments/to3Di.py
@@ -7,9 +7,10 @@ def c(cmd: str | list[str]) -> str:
     return subprocess.check_output(cmd, shell=True).decode()
 
 
-def createdb(dir="~/Desktop/proteins", db="./.temp/3DiAA"):
-    """Returns True if the database was created successfully, False otherwise."""
-    print(c(f"foldseek createdb {dir} {db}"))
+def foldseek(cmd: str, verbose=False):
+    logs = c(f"foldseek {cmd}")
+    if verbose:
+        print(logs)
 
 
 def parse_seq(line):
@@ -28,13 +29,7 @@ def parse_names(db):
         return idxs, names
 
 
-def parse_3Di(db):
-    with open(db + "_ss", "r") as infile:
-        data = [parse_seq(line) for line in infile.readlines()]
-        return data
-
-
-def parse_AA(db):
+def parse_seqs(db):
     with open(db, "r") as infile:
         data = [parse_seq(line) for line in infile.readlines()[:-1]]
         return data
@@ -47,20 +42,23 @@ class Parsed3DiAA:
     repr_3Di: list[str]
 
 
-def to_3DiAA(dir="~/Desktop/proteins", db="./.temp/3DiAA"):
+def to3Di(dir="~/Desktop/proteins", db="./.temp/3DiAA", verbose=False):
     os.makedirs(".temp", exist_ok=True)
     try:
-        createdb(dir, db)
+        foldseek(f"createdb {dir} {db}", verbose=verbose)
     except Exception as e:
         print(e)
         return
 
     _, names = parse_names(db)
-    repr_3Di = parse_3Di(db)
-    repr_AA = parse_AA(db)
+    repr_3Di = parse_seqs(db + "_ss")
+    repr_AA = parse_seqs(db)
+    assert (
+        len(repr_3Di) == len(repr_AA) == len(names)
+    ), "The number of sequences should be the same."
 
     return Parsed3DiAA(names=names, repr_amino_acids=repr_AA, repr_3Di=repr_3Di)
 
 
 if __name__ == "__main__":
-    to_3DiAA()
+    print(to3Di())
