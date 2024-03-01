4735b384caf68a6bf04bd6767edb48f657fd2c68
xnought <bertuccd@oregonstate.edu>
2024-03-01 15:40:07 -0800

    feat: connection between frontend and backend

---
+++ website/backend/server.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
+from pydantic import BaseModel, ConfigDict
+
+
+# https://github.com/zeno-ml/zeno-hub/blob/9d2f8b5841d99aeba9ec405b0bc6a5b1272b276f/backend/zeno_backend/classes/base.py#L20
+def to_camel(string: str) -> str:
+    """Converter for variables from snake_case to camelCase.
+
+    Args:
+        string (str): the variable to convert to camelCase.
+
+    Returns:
+        str: camelCase representation of the variable.
+    """
+    components = string.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+# https://github.com/zeno-ml/zeno-hub/blob/9d2f8b5841d99aeba9ec405b0bc6a5b1272b276f/backend/zeno_backend/classes/base.py#L20
+class CamelModel(BaseModel):
+    """Converting snake_case pydantic models to camelCase models."""
+
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)  # type: ignore
+
+
+def disable_cors(app: FastAPI, origins=["*"]):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return app
+
+
+# https://github.com/zeno-ml/zeno/blob/main/zeno/server.py#L52
+def custom_generate_unique_id(route: APIRoute):
+    return route.name
+
+
+def init_fastapi_app() -> FastAPI:
+    app = FastAPI(
+        title="protein-map backend",
+        generate_unique_id_function=custom_generate_unique_id,
+    )
+    return app
+
+
+app = init_fastapi_app()
+disable_cors(app, origins=["*"])
+
+
+class TestResponse(BaseModel):
+    nice: int
+
+
+@app.get("/", response_model=TestResponse)
+def test():
+    return TestResponse(nice=1)
---
+++ website/frontend/src/lib/openapi/services/DefaultService.ts
@@ -0,0 +1,21 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { TestResponse } from '../models/TestResponse';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class DefaultService {
+    /**
+     * Test
+     * @returns TestResponse Successful Response
+     * @throws ApiError
+     */
+    public static test(): CancelablePromise<TestResponse> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/',
+        });
+    }
+}
